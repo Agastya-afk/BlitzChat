237616b9fe43d55b34f03eb9f8cb30352c19bc1d
Agastya-afk <tripathiagastya56@gmail.com>
2026-05-21 12:20:26 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "blitz-chat-xecs.vercel.app" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "https://blitz-chat-xecs.vercel.app/api",
+
   withCredentials: true,
 });
